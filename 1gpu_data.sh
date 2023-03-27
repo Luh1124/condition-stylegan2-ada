579b414fb014421104e2b7389a9a0cfef524f894
Luh1124 <link0502@buaa.edu.cn>
2023-03-27 23:00:34 +0800

    data

--- 1gpu_data.sh
+++ 1gpu_data.sh
@@ -1,3 +1,3 @@
 # python dataset_tool.py --source=/home/momobot/repo/data/faceset/*/unwrap_texture --dest=/home/momobot/repo/data/faceset/unwrap_texture
 # python dataset_tool.py --source=/home/momobot/repo/data/faceset/ori/unwarp_texture --dest=/home/momobot/repo/data/faceset/unwarp_texture
-python dataset_tool.py --source=/home/momobot/repo/data/faceset/ori/edit --dest=/home/momobot/repo/data/faceset/edit
+python dataset_tool.py --source=/home/momobot/repo/data/faceset/ori/edit --dest=/home/momobot/repo/data/faceset/edit --width=256 --height=256
